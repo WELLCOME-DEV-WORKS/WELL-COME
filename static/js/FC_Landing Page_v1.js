c3ce979842c69f834608e2a82db1f6a19d809454
nayounghye <yhna.dev@gmail.com>
2024-09-24 12:22:57 +0900

    Feat: 섹션2 순차적으로 나오게 애니메이션 수정

--- static/js/FC_Landing Page_v1.js
+++ static/js/FC_Landing Page_v1.js
@@ -5,21 +5,28 @@
    afterLoad: function(origin, destination){
     const section1IMG = document.querySelector('#section1').querySelector('img');
        const section2H1 = document.querySelector('#section2').querySelector('h1');
-       const section2P = document.querySelector('#section2').querySelector('p');
+       const section2P = document.querySelectorAll('#section2 p');
        const section3BT = document.querySelector('#section3').querySelector('button');
        // reaching section 2?
        if(destination.index == 1){
         section1IMG.classList.add('active');
            section2H1.classList.add('active');
-           section2P.classList.add('active');
-           section3BT.classList.add('active');
+           // section2P.forEach(p => p.classList.add('active'));
+            // 각 p 태그에 순차적으로 active 클래스를 추가
+            section2P.forEach((p, index) => {
+               setTimeout(() => {
+                   p.classList.add('active');
+               }, index * 0.1); // 500ms 간격으로 active 클래스 추가
+           });
+           section3BT.classList.add('active'); 
+           
        }
 
        // back to original state
        else if(origin && origin.index == 1){
         section1IMG.classList.remove('active');
            section2H1.classList.remove('active');
-           section2P.classList.remove('active');
+           section2P.forEach(p => p.classList.remove('active'));
            section3BT.classList.remove('active');
        }
 
@@ -32,7 +39,7 @@
    menu: '#menu',
 
    navigation: true,
-navigationTooltips: ['First Section', 'Second Section', 'Third Section'],
+navigationTooltips: ['', '', ''],
 });
 
 // made with fullpage.js 제거
