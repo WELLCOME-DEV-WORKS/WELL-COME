39935907c0f156ac7795bbd9db7e44a7326a768a
nayounghye <yhna.dev@gmail.com>
2024-09-25 20:26:45 +0900

    Fix : 섹션1 이미지 범위넘는 현상 수정

--- static/js/FC_Landing Page_v1.js
+++ static/js/FC_Landing Page_v1.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
    easingcss3: 'ease',
    scrollingSpeed: 1000,
    // responsiveWidth: 920,
-   // responsiveHeight:900,
+   responsiveWidth: 1000, // 너비가 1000일때 수동 원페이지 -> 스크롤
      // Loop 설정
      loopTop: true,
      loopBottom: true,
@@ -20,31 +20,39 @@ $(document).ready(function() {
      navigationTooltips: ['', '', ''],
 
        
-       // 섹션이 로드될 때마다 실행
+       // afterLoad: 섹션이 로드될 때마다 실행
        afterLoad: function(anchorLink, index){
-         console.log('anchorLink.index:', anchorLink);
-         console.log('index:', index);
          const section1IMG = $('#fc-section1 img');
          const section2H1 = $('#fc-section2 h1');
          const section2P = $('#fc-section2 p');
          const section3BT = $('#fc-section3 button');
 
-           // section1 도착 시
+           // section1 도착!
            if (index === 1) {
-               section1IMG.addClass('active');       
+               // section1IMG.addClass('active');   
+               section1IMG.each(function() {
+                  $(this).addClass('reset-position');
+              });    
+               if (!sessionStorage.getItem('autoMoved')) {
+                  sessionStorage.setItem('autoMoved', 'true'); // 이동 플래그 설정
+
+                  // 3초 후에 section2로 자동 이동
+                  setTimeout(function() {
+                      $.fn.fullpage.moveTo('secondPage'); // section2로 이동
+                  }, 300); //0.3초 뒤 이동
+              }
+      else {
+         section1IMG.removeClass('reset-position');
+      }
            }
 
-           // section2 도착 시
+
+
+           // section2 도착!
            else if (index === 2) {
             section2H1.addClass('active');
-               // 각 p 태그에 순차적으로 active 클래스 추가
-               section2P.each(function(index, p){
-                   setTimeout(function(){
-                       $(p).addClass('active');
-                   }, index * 100); // 100ms 간격으로 active 클래스 추가
-               });
-
-               section3BT.addClass('active');
+            section2P.addClass('active');
+            section3BT.addClass('active');
            }
 
            // 원래 상태로 돌아가기
@@ -53,7 +61,6 @@ $(document).ready(function() {
            }
 
            else if (anchorLink === 'secondPage') {
-            console.log('anchorLink.index:', anchorLink.index);
                section2H1.removeClass('active');
                section2P.removeClass('active');
                section3BT.removeClass('active');
@@ -62,13 +69,15 @@ $(document).ready(function() {
    });
 });
 
+
 $(".why-item").each(function(){
    let text = this;
+   // 각 문자에 <i> 태그 추가
    text.innerHTML = text.textContent.replace(/\S/g, "<i>$&</i>")
    $(this).find("i").each(function(index, item){
      $(this).addClass("num" + index);
      let i = index / 14;
+     // 각 문자에 애니메이션 딜레이 추가
      $(this).css("animation-delay", (i + 0.8) + "s")
    })
  })
-
